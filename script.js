b2c6c9166ff05c9d8a2ddda296dd213b2cbfff62
unknown <gamejapann@gmail.com>
2023-08-14 21:21:01 +0900

    3rd commit

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ import { TinySegmenter } from "https://code4fukui.github.io/TinySegmenter/TinySe
 
 
 
-
 const fileInput = document.getElementById('file-input');
 const textDisplay = document.getElementById('text-display');
 const prevButton = document.getElementById('prev-button');
 const nextButton = document.getElementById('next-button');
 let sentences = [];
 let currentIndex = 0;
+let wordIndex = 0;
+let nextSwitchSentence = false;
+let prevSwitchSentence = false;
+let backToPrevSen = false;
+
+
+
 
 fileInput.addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -16,10 +22,11 @@ fileInput.addEventListener('change', (event) => {
         const reader = new FileReader();
         reader.onload = function (event) {
             const content = event.target.result;
-            const seg = TinySegmenter.segment(content);
-            console.log(seg);
+
+            // 句読点をもとに分割
             sentences = content.split(/[、,.。!！?？]/);
             currentIndex = 0;
+            wordIndex=0
             displayCurrentSentence();
         }
         reader.readAsText(file);
@@ -27,19 +34,83 @@ fileInput.addEventListener('change', (event) => {
 });
 
 function displayCurrentSentence() {
-    textDisplay.textContent = sentences[currentIndex];
+    //コメントアウトコード
+    // const seg = TinySegmenter.segment(content);
+
+    nextSwitchSentence=false;
+    prevSwitchSentence=false;
+
+    // 単語をもとに分割
+    var content=sentences[currentIndex]
+
+    const segmenter = new Intl.Segmenter("ja", { granularity: "word" });
+    const segments = segmenter.segment(content);
+    // var reg = new RegExp(/^[!"%')\*\+\-\.,\/:;>?@\\\]^_`|}~？！。、」』】はがのをにへとで]$/);
+    var reg = new RegExp(/^[はがのをにへとで]$/);
+    
+    let zyoshiIndex=[]
+    zyoshiIndex.push(-1)
+    for (let seg of segments) {
+        if(reg.test(seg.segment)){
+            zyoshiIndex.push(seg.index)
+        }
+    }
+    zyoshiIndex.push(content.length-1)
+    let bunkatsu = []
+    for (let i = 0; i < zyoshiIndex.length-1; i++) {
+        bunkatsu.push(content.slice(zyoshiIndex[i]+1,zyoshiIndex[i+1]+1))
+        
+    }
+
+
+
+    // textDisplay.textContent = sentences[currentIndex];
+    if(backToPrevSen==true){
+        console.log("sadou")
+
+        wordIndex = bunkatsu.length-1
+        backToPrevSen=false
+    }
+
+
+
+    textDisplay.textContent = bunkatsu[wordIndex]
+    console.log(bunkatsu)
+
+    if(wordIndex == bunkatsu.length-1){
+        nextSwitchSentence=true;
+    }
+
+    if(wordIndex == 0){
+        prevSwitchSentence=true;
+    }
 }
 
+
+
+//slide部分
 prevButton.addEventListener('click', () => {
-    if (currentIndex > 0) {
+
+    if (prevSwitchSentence==true && currentIndex > 0) {
         currentIndex--;
+        backToPrevSen = true
+        displayCurrentSentence();
+    }else{
+        wordIndex--;
+        if (wordIndex <= 0){
+            wordIndex = 0
+        }
         displayCurrentSentence();
     }
 });
 
 nextButton.addEventListener('click', () => {
-    if (currentIndex < sentences.length - 1) {
+    if (nextSwitchSentence==true && currentIndex < sentences.length - 1) {
         currentIndex++;
+        wordIndex=0
+        displayCurrentSentence();
+    }else{
+        wordIndex++;
         displayCurrentSentence();
     }
 });
